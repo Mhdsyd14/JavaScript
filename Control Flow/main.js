a67f761363f1222c1dda03d7cbccfde1d7a6e0ea
DESKTOP-C2OUG3H\MhdSyd014 <irsyadmuhammad954@gmail.com>
2024-04-24 14:06:51 +0700

    Tugas javascript control flow program konversi nilai

---
+++ Control Flow/main.js
@@ -0,0 +1,12 @@
+let angka = prompt("Input Nilai");
+
+if (angka > 80) {
+  alert("Nilai anda A");
+} else if (angka > 70) {
+  alert("Nilai anda B");
+} else if (angka > 50) {
+  alert("Nilai anda C");
+} else if (angka > 30) {
+  alert("Nilai anda D");
+}
+alert("Nilai anda E");
