34597b68241df9260f418075290ee8acde17df8d
Mhdsyd14 <137772704+Mhdsyd14@users.noreply.github.com>
2024-06-02 21:06:14 +0700

    Revisi control flow

--- Control Flow/main.js
+++ Control Flow/main.js
@@ -2,11 +2,12 @@ let angka = prompt("Input Nilai");
 
 if (angka > 80) {
   alert("Nilai anda A");
-} else if (angka > 70) {
-  alert("Nilai anda B");
 } else if (angka > 50) {
-  alert("Nilai anda C");
+  alert("Nilai anda B");
 } else if (angka > 30) {
+  alert("Nilai anda C");
+} else if (angka > 10) {
   alert("Nilai anda D");
+} else if (angka > 0) {
+  alert("Nilai anda E");
 }
-alert("Nilai anda E");
