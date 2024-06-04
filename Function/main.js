5004ce5d1579b0ebee041e5a08863de5994def2a
Mhdsyd14 <137772704+Mhdsyd14@users.noreply.github.com>
2024-06-04 09:57:42 +0700

    Perubahan permainan game

--- Function/main.js
+++ Function/main.js
@@ -6,64 +6,66 @@ alert(
 function generateRandomValue() {
   return Math.floor(Math.random() * 3) + 1;
 }
+
 let jawabanBenerP1 = 0;
-let JawabanBenerP2 = 0;
+let jawabanBenerP2 = 0;
 
 // Fungsi untuk melakukan satu ronde permainan
 function playRound(round) {
   const randomValue = generateRandomValue();
-  const player1 = prompt(
-    `Player 1: Masukkan angka (1-3) jawabannya ${randomValue}`
-  );
+  const player1 = prompt(`Player 1: Masukkan angka (1-3)`);
   const player2 = prompt("Player 2: Masukkan angka (1-3)");
 
   // Periksa input pemain
   if (player1 === player2) {
     alert("Tebakan tidak boleh sama.");
+    round--;
   } else if (player1 < 1 || player1 > 3 || player2 < 1 || player2 > 3) {
-    alert("hasil tidak boleh lebih dari 3 atau kurang dari satu");
-
-    // playGame(round);
+    alert("Angka harus antara 1 dan 3 tidak boleh lebih dari itu.");
     round--;
   } else if (player1 != randomValue && player2 != randomValue) {
-    alert("hasil seri");
+    alert("Hasil seri.");
+    round--;
   } else {
     if (player1 == randomValue) {
-      alert("Player 1 benar!");
-      jawabanBenerP1++;
+      jawabanBenerP1 += 1;
+      alert(
+        `Player 1 benar! Skor saat ini - Player 1: ${jawabanBenerP1} dan Player 2: ${jawabanBenerP2}`
+      );
     }
     if (player2 == randomValue) {
-      alert("Player 2 benar!");
-      JawabanBenerP2++;
+      jawabanBenerP2 += 1;
+      alert(
+        `Player 2 benar! Skor saat ini - Player 1: ${jawabanBenerP1} dan Player 2: ${jawabanBenerP2}`
+      );
     }
   }
 
   // Tampilkan ronde
-  if (round == 6) {
-    if (jawabanBenerP1 > JawabanBenerP2) {
+  if (round === 6) {
+    if (jawabanBenerP1 > jawabanBenerP2) {
       return alert(
-        `Permainan selesai yang menang p1 dengan skor ${jawabanBenerP1} dan p2 dengan skor ${JawabanBenerP2}`
+        `Permainan selesai. Player 1 menang dengan skor player 1 adalah  ${jawabanBenerP1} dan player 2 adalah  ${jawabanBenerP2}`
       );
-    } else if (JawabanBenerP2 > jawabanBenerP1) {
+    } else if (jawabanBenerP2 > jawabanBenerP1) {
       return alert(
-        `Permainan selesai yang menang p2 dengan skor ${JawabanBenerP2} dan p1 dengan skor ${jawabanBenerP1}`
+        `Permainan selesai. Player 2 menang dengan skor player 2 adalah  ${jawabanBenerP2} dan player 1 adalah ${jawabanBenerP1}`
       );
     } else {
-      return alert("serii");
+      return alert(
+        `Permainan selesai. Hasilnya seri dengan skor player 1 =  ${jawabanBenerP1} dan player 2 = ${jawabanBenerP2}`
+      );
     }
   }
 
-  alert(`Ronde ${round}`);
-  round++;
-  playGame(round);
+  alert(`Ronde ${round} `);
+  playGame(round + 1);
 }
 
 // Fungsi untuk menjalankan seluruh permainan
-function playGame(a) {
-  // for (let round = a; round <= 6; round++) {
-  playRound(a);
-  // }
+function playGame(round) {
+  playRound(round);
 }
 
-// Mulai permainan
+// Mulai permainan dari ronde 1
 playGame(2);
